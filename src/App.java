1472d46c4fa72151ec472ff331e5293a952817f6
jvsco <jvscottydacosta@gmail.com>
2025-09-04 13:43:06 -0300

    6 commit

--- src/App.java
+++ src/App.java
@@ -4,38 +4,41 @@ public class App {
         System.out.println("--- Sistema de Clínica Veterinária ---");
 
 
-        Tutor tutor1 = new Tutor("Ana Silva", "123456789", "Rua A");
-        Tutor tutor2 = new Tutor("Pedro Costa", "987654321", "Avenida B");
+        Tutor[] tutores = new Tutor[2];
+        tutores[0] = new Tutor("Ana Silva", "123456789", "Rua A");
+        tutores[1] = new Tutor("Pedro Costa", "987654321", "Avenida B");
 
-        Animal animal1 = new Animal("Rex", "Cachorro", 5, "Histórico: vacinas em dia");
-        Animal animal2 = new Animal("Miau", "Gato", 2, "Histórico: consulta recente");
-        Animal animal3 = new Animal("Bolinha", "Cachorro", 3, "Histórico: sem problemas");
+        Animal[] animais = new Animal[3];
+        animais[0] = new Animal("Rex", "Cachorro", 5, "Histórico: vacinas em dia");
+        animais[1] = new Animal("Miau", "Gato", 2, "Histórico: consulta recente");
+        animais[2] = new Animal("Bolinha", "Cachorro", 3, "Histórico: sem problemas");
 
-        Veterinario vet1 = new Veterinario("Dr. João", "CRMV-1234", "Clínico Geral");
-        Veterinario vet2 = new Veterinario("Dra. Maria", "CRMV-5678", "Dermatologia");
-        Veterinario vet3 = new Veterinario("Dr. Carlos", "CRMV-9012", "Ortopedia");
+        Veterinario[] veterinarios = new Veterinario[3];
+        veterinarios[0] = new Veterinario("Dr. João", "CRMV-1234", "Clínico Geral");
+        veterinarios[1] = new Veterinario("Dra. Maria", "CRMV-5678", "Dermatologia");
+        veterinarios[2] = new Veterinario("Dr. Carlos", "CRMV-9012", "Ortopedia");
 
 
-        tutor1.adicionarAnimal(animal1);
-        tutor1.adicionarAnimal(animal2);
-        tutor2.adicionarAnimal(animal3);
+        tutores[0].adicionarAnimal(animais[0]);
+        tutores[0].adicionarAnimal(animais[1]);
+        tutores[1].adicionarAnimal(animais[2]);
 
         System.out.println("\n--- Animais dos Tutores ---");
-        tutor1.listarAnimais();
-        tutor2.listarAnimais();
+        tutores[0].listarAnimais();
+        tutores[1].listarAnimais();
 
 
-        vet2.setSupervisor(vet1);
-        vet3.setSupervisor(vet1);
+        veterinarios[1].setSupervisor(veterinarios[0]);
+        veterinarios[2].setSupervisor(veterinarios[0]);
 
         System.out.println("\n--- Dados dos Veterinários ---");
-        System.out.println(vet1.retornaDados());
-        System.out.println(vet2.retornaDados());
-        System.out.println(vet3.retornaDados());
+        System.out.println(veterinarios[0].retornaDados());
+        System.out.println(veterinarios[1].retornaDados());
+        System.out.println(veterinarios[2].retornaDados());
 
 
         System.out.println("\n--- Criando um Atendimento ---");
-        Atendimento atendimento1 = new Atendimento("2025-09-04", "Consulta de rotina", vet1);
+        Atendimento atendimento1 = new Atendimento("2025-09-04", "Consulta de rotina", veterinarios[0]);
         atendimento1.adicionarProcedimento(new Procedimento("Exame de Sangue", 100.00, "Resultados normais"));
         atendimento1.adicionarProcedimento(new Procedimento("Vacina Antirrábica", 50.00, ""));
 
@@ -43,7 +46,7 @@ public class App {
 
 
         System.out.println("\n--- Criando outro Atendimento ---");
-        Atendimento atendimento2 = new Atendimento("2025-09-04", "Consulta de pele", vet2);
+        Atendimento atendimento2 = new Atendimento("2025-09-04", "Consulta de pele", veterinarios[1]);
         atendimento2.adicionarProcedimento(new Procedimento("Exame Dermatológico", 80.00, ""));
 
         atendimento2.exibirAtendimento();
